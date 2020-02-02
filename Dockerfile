63fc166b9cbbf377d79220366565605b1971b0cf
Stanislav Novotný <stanislav.novotny@unicorn.com>
2020-02-02 15:52:34 +0100

    Support for Auction Specification

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-FROM tomcat:9-jre8
+FROM maven:3-jdk-8
+RUN git clone https://github.com/nWmCZ/TPL18.git /TPL18.git
+WORKDIR /TPL18.git
+RUN mvn clean install
 
+FROM openjdk:8-alpine
 ARG BUILD_DATE
 
 ARG VCS_REF
@@ -10,11 +14,8 @@ LABEL   org.label-schema.build-date=$BUILD_DATE \
         org.label-schema.schema-version="1.0.0-rc.1"
 
 EXPOSE 8080
-
+COPY --from=0 /TPL18.git/target/tpl18-0.0.1-SNAPSHOT.jar /
+COPY ./src/main/resources/application.properties /Work/tpl/
 RUN mkdir -p /Work/tpl/in /Work/tpl/out /Work/tpl/in_processed /Work/tpl/out_ack
 
-COPY ./devops/tomcat/setenv.sh /usr/local/tomcat/bin/setenv.sh
-
-COPY ./src/main/resources/application.properties /Work/tpl/
-
-COPY ./target/tpl*.war /usr/local/tomcat/webapps/tpl.war
+ENTRYPOINT ["/usr/bin/java", "-jar", "/tpl18-0.0.1-SNAPSHOT.jar", "-Dspring.config.location=/Work/tpl/application.properties"]
--- src/main/java/eu/sn/parse/XMLParser.java
+++ src/main/java/eu/sn/parse/XMLParser.java
@@ -15,29 +15,46 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.UUID;
 
 @Service
 public class XMLParser {
     public Message parseXML(File file) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        dbFactory.setNamespaceAware(true);
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(file);
-
         //optional, but recommended
         //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
         doc.getDocumentElement().normalize();
 
-        String mRID = doc.getDocumentElement().getElementsByTagName("mRID").item(0).getTextContent();
-        String version = doc.getDocumentElement().getElementsByTagName("revisionNumber").item(0).getTextContent();
-        String createdDateTime = doc.getDocumentElement().getElementsByTagName("createdDateTime").item(0).getTextContent();
-        String senderMarketParticipant = doc.getDocumentElement().getElementsByTagName("sender_MarketParticipant.mRID").item(0).getTextContent();
-        String senderMarketParticipantType = doc.getDocumentElement().getElementsByTagName("sender_MarketParticipant.marketRole.type").item(0).getTextContent();
-        String receiverMarketParticipant = doc.getDocumentElement().getElementsByTagName("receiver_MarketParticipant.mRID").item(0).getTextContent();
-        String receiverMarketParticipantType = doc.getDocumentElement().getElementsByTagName("receiver_MarketParticipant.marketRole.type").item(0).getTextContent();
-
+        String mRID = "";
+        String version = "";
+        if (null != doc.getDocumentElement().getElementsByTagNameNS("*","mRID").item(0)) {
+            mRID = doc.getDocumentElement().getElementsByTagNameNS("*","mRID").item(0).getTextContent();
+        }
+        if (null != doc.getDocumentElement().getElementsByTagNameNS("*","revisionNumber").item(0)) {
+            version = doc.getDocumentElement().getElementsByTagNameNS("*","revisionNumber").item(0).getTextContent();
+        }
+        String createdDateTime = doc.getDocumentElement().getElementsByTagNameNS("*", "createdDateTime").item(0).getTextContent();
+        String senderMarketParticipant = doc.getDocumentElement().getElementsByTagNameNS("*","sender_MarketParticipant.mRID").item(0).getTextContent();
+        String senderMarketParticipantType = doc.getDocumentElement().getElementsByTagNameNS("*","sender_MarketParticipant.marketRole.type").item(0).getTextContent();
+        String receiverMarketParticipant = doc.getDocumentElement().getElementsByTagNameNS("*","receiver_MarketParticipant.mRID").item(0).getTextContent();
+        String receiverMarketParticipantType = "";
+        if (null != doc.getDocumentElement().getElementsByTagNameNS("*","receiver_MarketParticipant.marketRole.type").item(0)) {
+            receiverMarketParticipantType = doc.getDocumentElement().getElementsByTagNameNS("*","receiver_MarketParticipant.marketRole.type").item(0).getTextContent();
+        }
         Message message = new Message();
-        message.setDocumentId(mRID);
-        message.setVersion(Integer.valueOf(version));
+        if (mRID.length() == 0) {
+            message.setDocumentId(UUID.randomUUID().toString().replace("-", ""));
+        } else {
+            message.setDocumentId(mRID);
+        }
+
+        if (version.length() != 0) {
+            message.setVersion(Integer.valueOf(version));
+        }
+
         // 2017-11-21T15:21:21Z
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH);
         Date date = null;
